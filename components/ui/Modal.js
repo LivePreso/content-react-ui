6bf4f457ede930129e438109fa46e660795e56f4
Lauren Rui <lauren.rui@livepreso.com>
2024-02-20 21:56:58 +1100

    feat(UI): add basic modal component

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -1,25 +1,29 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/interactive-supports-focus, jsx-a11y/role-has-required-aria-props */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import style from './Modal.module.scss';
 
-export function Modal({ open, children, className }) {
-  const classes = classNames(className, style.modal, {
-    [style.isOpen]: open,
-  });
+export function Modal({ children, className, onClose }) {
+  const classes = classNames(className, style.modal);
 
-  return <div className={classes}>{children}</div>;
+  return (
+    <div className={style.wrapper}>
+      <div className={style.shroud} onClick={onClose} type="button" />
+      <div className={classes}>{children}</div>
+    </div>
+  );
 }
 
 Modal.propTypes = {
-  open: PropTypes.bool.isRequired,
-  // onClose: PropTypes.func,
+  onClose: PropTypes.func,
   children: PropTypes.node,
   className: PropTypes.string,
 };
 
 Modal.defaultProps = {
-  // onClose: () => {},
+  onClose: () => {},
   children: null,
   className: '',
 };
