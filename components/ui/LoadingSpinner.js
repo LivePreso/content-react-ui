434ece813577d332a9293ad2cb43fadfe2748ef2
Lauren Rui <lauren.rui@livepreso.com>
2024-02-22 16:16:11 +1100

    feat(LoadingSpinner): add width and height props

--- components/ui/LoadingSpinner.js
+++ components/ui/LoadingSpinner.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import style from './LoadingSpinner.module.scss';
 
-export function LoadingSpinner() {
-  return <div className={style.loadingSpinner} />;
+export function LoadingSpinner({ width, height }) {
+  return <div style={{ width, height }} className={style.loadingSpinner} />;
 }
+
+LoadingSpinner.propTypes = {
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+};
+
+LoadingSpinner.defaultProps = {
+  width: '100%',
+  height: '100%',
+};
