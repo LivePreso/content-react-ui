0088a70dbd8b240c7f49dca35c8235d3f869c618
Lauren Rui <lauren.rui@livepreso.com>
2024-10-18 18:00:05 +1100

    feat(Flex): add width and height style options

--- components/layout/Flex.js
+++ components/layout/Flex.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { flexPropTypes } from './flex-prop-types';
 
-export function Flex({ className, flex, children }) {
+export function Flex({ className, flex, width, height, children }) {
   const classes = classNames(className);
 
   return (
-    <div className={classes} style={{ flex }}>
+    <div className={classes} style={{ flex, width, height }}>
       {children}
     </div>
   );
@@ -14,8 +15,16 @@ export function Flex({ className, flex, children }) {
 
 Flex.propTypes = {
   flex: flexPropTypes,
+  className: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  children: PropTypes.node,
 };
 
 Flex.defaultProps = {
   flex: 1,
+  className: '',
+  width: null,
+  height: null,
+  children: null,
 };
