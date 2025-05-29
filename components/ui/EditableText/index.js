abf30f94f1de1e3ce9666e22f7b9274c89bc3aaf
Hugh Campbell <hugh.campbell@livepreso.com>
2025-05-29 15:59:27 +1000

    add empty array as default for toolbar when destructuring props

--- components/ui/EditableText/index.js
+++ components/ui/EditableText/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
+
 import { BLOCK_LEVEL_FORMATS, DEFAULT_PROPS, PROP_TYPES } from './constants';
 import { EditableText as EditableTextBase } from './EditableText';
 import { EditableTextReadonly } from './EditableTextReadonly';
 
-export function EditableText({ tag, toolbar, isReadOnly, ...props }) {
+export function EditableText({ tag, toolbar = [], isReadOnly, ...props }) {
   // Block-level formatting is restricted to 'div' tags
   const toolbarOptions =
     tag === 'div'
