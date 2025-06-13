330ce8c32d7c72818d843d5e6453d42609833bb2
Lauren Rui <lauren.rui@livepreso.com>
2025-06-13 14:58:35 +1000

    feat(EditableInput): Add more props
    - formatter
    - more styling classes
    - type (allow text vs. number)

--- components/ui/EditableInput.js
+++ components/ui/EditableInput.js
@@ -4,37 +4,69 @@ import classNames from 'classnames';
 import { EditText } from 'react-edit-text';
 import style from './EditableInput.module.scss';
 
-export function EditableInput(props) {
-  const { tagName, readOnly, value, onChange } = props;
-
+export function EditableInput({
+  type,
+  tagName,
+  readOnly,
+  value,
+  onChange,
+  formatter,
+  className,
+  inputClassName,
+}) {
   // Render the real tag instead that our EditText is trying to represent.
   if (readOnly) {
     const ReactTag = `${tagName}`;
-    return <ReactTag className={style.readonly}>{value}</ReactTag>;
+    return (
+      <ReactTag className={classNames(style.readonly, className)}>
+        {value}
+      </ReactTag>
+    );
+  }
+
+  const extraProps = {};
+
+  if (formatter) {
+    extraProps.formatDisplayText = formatter;
   }
 
   return (
     <EditText
+      type={type}
       value={value}
-      className={classNames(style.edit, style[tagName])}
-      inputClassName={classNames(style.input, style[tagName])}
+      className={classNames(style.edit, style[tagName], className)}
+      inputClassName={classNames(
+        style.input,
+        style[tagName],
+        className,
+        inputClassName,
+      )}
       onChange={(e) => {
         onChange(e.target.value);
       }}
+      {...extraProps}
     />
   );
 }
 
 EditableInput.propTypes = {
   value: PropTypes.string,
+  type: PropTypes.oneOf(['text', 'number']),
   onChange: PropTypes.func,
   readOnly: PropTypes.bool,
   tagName: PropTypes.oneOf(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  formatter: PropTypes.func,
+  className: PropTypes.string,
+  inputClassName: PropTypes.string,
 };
 
 EditableInput.defaultProps = {
   value: '',
+  type: 'text',
   onChange: () => {},
   tagName: 'p',
   readOnly: false,
+  formatter: null,
+  className: null,
+  inputClassName: null,
 };
